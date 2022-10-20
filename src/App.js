e0f473e467463c7d26c82cfe8d21e49c1ada6d9d
sanath nath <sanathnadh54@gmail.com>
2022-10-20 21:02:21 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       // console.log(currentLocation);
       axios
         .get(
-          "//api.weatherstack.com/forecast?access_key=9b9d020fda6189eb11a1c3b54f276778&query=" +
+          "http://api.weatherstack.com/forecast?access_key=9b9d020fda6189eb11a1c3b54f276778&query=" +
             str
         )
         .then((res) => {
